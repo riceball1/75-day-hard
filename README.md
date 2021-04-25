c4782d91efc998d051dd6e8a5c6edf72538c3a30
Dana Ng <ngdana1@gmail.com>
2021-04-25 10:40:12 -0700

    Added files for setup

---
+++ README.md
@@ -0,0 +1,27 @@
+# 75-day-hard
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Setup 
+- How to Deploy Vue Github Pages - https://blog.logrocket.com/build-deploy-vue-js-app-github-pages/
---
+++ src/components/MainPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>A tracker for 75 Day Hard Challenge</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
